b54960b73ebbce8c6b1c52192a5a41f4d59f3697
jcleira <jmc.leira@gmail.com>
2021-01-18 12:00:46 +0100

    Fix issues and start adding the proper Job Spec

--- controllers/registry_controller.go
+++ controllers/registry_controller.go
@@ -25,6 +25,7 @@ import (
 	"github.com/go-logr/logr"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -33,11 +34,23 @@ import (
 	awsv1 "github.com/jcleira/ecr-credentials-controller/api/v1"
 )
 
+// clock knows how to get the current time.
+// It can be used to fake out timing for testing.
+type Clock interface {
+	Now() time.Time
+}
+
+type realClock struct{}
+
+func (_ realClock) Now() time.Time { return time.Now() }
+
 // RegistryReconciler reconciles a Registry object
 type RegistryReconciler struct {
 	client.Client
 	Log    logr.Logger
 	Scheme *runtime.Scheme
+
+	Clock
 }
 
 var (
@@ -144,12 +157,12 @@ func (r *RegistryReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	// Deleting old failed jobs, leaving the last one as reference.
 	if len(failedJobs) > 1 {
 		sort.Slice(failedJobs, func(i, j int) bool {
-			if failedJobs[i].Status.LastRefreshTime == nil {
-				return failedJobs[j].Status.LastRefreshTime != nil
+			if failedJobs[i].Status.StartTime == nil {
+				return failedJobs[j].Status.StartTime != nil
 			}
 
-			return failedJobs[i].Status.LastRefreshTime.Before(
-				failedJobs[j].Status.LastRefreshTime,
+			return failedJobs[i].Status.StartTime.Before(
+				failedJobs[j].Status.StartTime,
 			)
 		})
 
@@ -172,12 +185,12 @@ func (r *RegistryReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	// Deleting old jobs, leaving the last one as reference.
 	if len(successfulJobs) > 1 {
 		sort.Slice(successfulJobs, func(i, j int) bool {
-			if successfulJobs[i].Status.LastRefreshTime == nil {
-				return successfulJobs[j].Status.LastRefreshTime != nil
+			if successfulJobs[i].Status.StartTime == nil {
+				return successfulJobs[j].Status.StartTime != nil
 			}
 
-			return successfulJobs[i].Status.LastRefreshTime.Before(
-				successfulJobs[j].Status.LastRefreshTime,
+			return successfulJobs[i].Status.StartTime.Before(
+				successfulJobs[j].Status.StartTime,
 			)
 		})
 
@@ -197,7 +210,7 @@ func (r *RegistryReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		}
 	}
 
-	getNextSchedule := func(registry *awsv1.Registry, now time.Time) time.Time {
+	getNextSchedule := func(registry awsv1.Registry, now time.Time) time.Time {
 		if registry.Status.LastRefreshTime != nil {
 			return registry.Status.LastRefreshTime.Time.Add(time.Hour * 10)
 		}
@@ -207,32 +220,107 @@ func (r *RegistryReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 	nextSchedule := getNextSchedule(registry, r.Now())
 
-	if nextSchedule.Before(now) {
+	if nextSchedule.Before(r.Now()) {
 		return ctrl.Result{RequeueAfter: nextSchedule.Sub(r.Now())}, nil
 	}
 
 	buildJobforRegistry := func(
-		registry *awsv1.Registry, scheduledTime time.Time) (*kbatch.Job, error) {
+		registry *awsv1.Registry, scheduledTime time.Time) (*batchv1.Job, error) {
 		// We want job names for a given nominal start time to have a
 		// deterministic name to avoid the same job being created twice.
 		name := fmt.Sprintf("%s-%d", registry.Name, scheduledTime.Unix())
 
-		job := &kbatch.Job{
+		job := &batchv1.Job{
 			ObjectMeta: metav1.ObjectMeta{
 				Labels:      make(map[string]string),
 				Annotations: make(map[string]string),
 				Name:        name,
 				Namespace:   registry.Namespace,
 			},
-			Spec: *registry.Spec.JobTemplate.Spec.DeepCopy(),
+			Spec: batchv1.JobSpec{
+				/*
+					Parallelism:             1,
+					Completions:             1,
+					ActiveDeadlineSeconds:   5,
+					BackoffLimit:            3,
+					TTLSecondsAfterFinished: 500,
+				*/
+				Template: &v1.PodTemplateSpec{
+					Spec: &v1.PodSpec{
+						Containers: []Container{
+							{
+								Name:            "ecr-registry-credentials",
+								Image:           "jcorral/awscli-kubectl",
+								ImagePullPolicy: "IfNotPresent",
+							},
+						},
+
+						/*
+							apiVersion: batch/v1beta1
+							kind: CronJob
+							metadata:
+								name: ecr-registry-credentials
+							spec:
+								concurrencyPolicy: Allow
+								failedJobsHistoryLimit: 1
+								jobTemplate:
+									spec:
+										template:
+											spec:
+												containers:
+												- name: ecr-registry-credentials
+													image: jcorral/awscli-kubectl:latest
+													imagePullPolicy: IfNotPresent
+													command:
+													- /bin/sh
+													- -c
+													- |-
+														SECRET_NAME=${AWS_REGION}-ecr-registry-credentials
+														EMAIL=no@local.info
+														TOKEN=`aws ecr get-login-password --region ${AWS_REGION}`
+														kubectl delete secret --ignore-not-found ${SECRET_NAME}
+														kubectl create secret docker-registry ${SECRET_NAME} \
+														--docker-server=https://${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com \
+														--docker-username=AWS \
+														--docker-password="${TOKEN}" \
+														--docker-email="${EMAIL}"
+														kubectl patch sa default -p '{"imagePullSecrets":[{"name":"'${SECRET_NAME}'"}]}'
+													env:
+													- name: AWS_REGION
+														value: eu-west-1
+													- name: AWS_ACCOUNT
+														valueFrom:
+															secretKeyRef:
+																name: aws-credentials
+																key: AWS_ACCOUNT
+													- name: AWS_ACCESS_KEY_ID
+														valueFrom:
+															secretKeyRef:
+																name: aws-credentials
+																key: AWS_ACCESS_KEY_ID
+													- name: AWS_SECRET_ACCESS_KEY
+														valueFrom:
+															secretKeyRef:
+																name: aws-credentials
+																key: AWS_SECRET_ACCESS_KEY
+												restartPolicy: Never
+												serviceAccount: ecr-registry-credentials
+												serviceAccountName: ecr-registry-credentials
+												terminationGracePeriodSeconds: 30
+												schedule: 0 *\/6 * * *
+												successfulJobsHistoryLimit: 3
+												suspend: false
+						*/
+					},
+				},
+			},
 		}
-		for k, v := range registry.Spec.JobTemplate.Annotations {
-			job.Annotations[k] = v
-		}
+
 		job.Annotations[scheduledTimeAnnotation] = scheduledTime.Format(time.RFC3339)
 		for k, v := range registry.Spec.JobTemplate.Labels {
 			job.Labels[k] = v
 		}
+
 		if err := ctrl.SetControllerReference(registry, job, r.Scheme); err != nil {
 			return nil, err
 		}
